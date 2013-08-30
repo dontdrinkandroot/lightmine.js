1c336fa08d4a543891ffc6e7e19e8930699d5f6f
Philip Washington Sorst <philip@sorst.net>
2013-08-30 13:06:58 +0200

    Adding loading spinner

--- js/directives.js
+++ js/directives.js
@@ -1,1 +1,22 @@
 var directives = angular.module('LightmineApp.directives', []);
+
+directives.directive('spinner', function() {
+	return {
+		scope: {
+			scopeVar: "=spinner",
+		},
+		restrict: 'A',
+		replace: false,
+		template: '<img src="images/spinner.gif" />',
+		link: function($scope, elm, attrs) {
+			
+			$scope.$watch("scopeVar", function() {
+				if ($scope.scopeVar !== undefined) {
+					elm.hide();
+				} else {
+					elm.show();
+				}
+			});
+		}
+	};
+});
