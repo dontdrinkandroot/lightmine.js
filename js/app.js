925b82851c2a7b3b03ff8fb283334fdf6c043a84
Philip Washington Sorst <philip@sorst.net>
2015-01-02 22:52:27 +0100

    Building request/response interceptor to (un)wrap redmine objects, refs #21

--- js/app.js
+++ js/app.js
@@ -54,6 +54,20 @@ angular.module('LightmineApp', [ 'LightmineApp.filters', 'LightmineApp.services'
     .run(function ($rootScope, $location, Restangular) {
 
         Restangular.setRequestSuffix('.json');
+        Restangular.addResponseInterceptor(function (data, operation, what, url, response, deferred) {
+            /* Redmine does wrap the responses, so we need to extract them in oder to "restangularize" */
+            if (what === "issues") {
+                if (operation === "get") {
+                    return data.issue;
+                }
+            }
+            console.log("Umodified Response", data, operation, what, url, response, deferred);
+            return data;
+        });
+        Restangular.addRequestInterceptor(function (element, operation, what, url) {
+            console.log("Unmodified Request", element, operation, what, url);
+            return element;
+        });
 
         /* Remember path that was originally requested so we can redirect there after login */
         $rootScope.requestedPath = $location.path();
